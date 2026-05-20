739a52d44396482be8aa290b150a7d21a337ee52
igor <igorc7871@gmail.com>
2026-05-20 15:11:11 -0300

    fix : corrigindo erros de sintaxe na model Usuario

--- LojaProjeto/Models/Usuario.cs
+++ LojaProjeto/Models/Usuario.cs
@@ -2,5 +2,11 @@
 {
     public class Usuario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+
+        public string Nivel { get; set; } = "Operador";
     }
 }
