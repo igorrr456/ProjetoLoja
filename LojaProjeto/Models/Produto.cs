6fa2b95926cff3b55c74278d7039192fccf8aca5
igor <igorc7871@gmail.com>
2026-05-20 14:12:22 -0300

    fix: Corrigindo erros de sintaxe na model

--- LojaProjeto/Models/Produto.cs
+++ LojaProjeto/Models/Produto.cs
@@ -2,5 +2,9 @@
 {
     public class Produto
     {
+       public int Id { get; set; }
+       public string Nome { get; set; } = string.Empty;
+       public decimal Preco { get; set; }
+       public string Categoria { get; set; } = string.Empty;
     }
 }
