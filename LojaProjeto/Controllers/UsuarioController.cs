43f5928aa86c2d31bd890757bf0f0e980e37f9cc
igor <igorc7871@gmail.com>
2026-05-21 16:51:50 -0300

    chore: configurando a Controller Usuario

---
+++ LojaProjeto/Controllers/UsuarioController.cs
@@ -0,0 +1,62 @@
+﻿using LojaProjeto.Interfaces;
+using LojaProjeto.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LojaProjeto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Logar() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logar(Usuario model)
+        {
+            if (ModelState.IsValid) return View(model);
+            var usuario = _usuarioRepositorio.Validar(model.Email, model.Senha);
+
+            if (usuario != null)
+            {
+                var claims = new List<Claim>
+                {
+                new Claim(ClaimTypes.Name, usuario.Nome),
+                new Claim(ClaimTypes.Email, usuario.Email),
+                new Claim("Nivel de acesso", usuario.Nivel),
+                new Claim("UsuarioId", usuario.Id.ToString())
+                };
+                var claimsIdentity = new ClaimsIdentity(claims , CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync
+                    (
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    new AuthenticationProperties { IsPersistent = false }
+                    );
+                return RedirectToAction("Index", "Home");   
+
+            }
+            ModelState.AddModelError("", "E-mail ou senha inválidos ");
+            return View(model);
+        }
+
+        public async Task<IActionResult> Sair()
+        {
+            
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            
+            //Manda o usuário de volta para a tela de login.
+            return RedirectToAction("Logar");
+        }
+    }
+}
